e3ad5dc8629002964ecbf46e19f9d50d29725d77
tbullmann <bullmann.torsten@gmail.com>
2016-11-11 17:01:33 +0900

    First commit for analysis of structural networks.

--- publication/figure_09.py
+++ publication/figure_09.py
@@ -37,10 +37,41 @@ def explore_parameter_space_for_structural_connectivity():
     print 'Saved data'
 
 
-# Plotting
+def analyse_structural_networks():
+    network = pickle.load(open('temp/struc_networks_for_thr_peak_thr_overlap_hidens2018.p', 'rb'))
+    factors, thresholds = (list(sorted(set(index)) for index in zip(*list(network))))
+
+    k = np.zeros((len(factors), len(thresholds)))
+    C = np.zeros((len(factors), len(thresholds)))
+    L = np.zeros((len(factors), len(thresholds)))
+    D = np.zeros((len(factors), len(thresholds)))
+    G = nx.DiGraph()
+
+    for i,thr_peak in enumerate(factors):
+        for j,thr_overlap in enumerate(thresholds):
+                edges = list(key for key in network[(thr_peak, thr_overlap)]['overlap ratio'])
+                G.clear()
+                G.add_edges_from(edges)
+                giant = max(nx.connected_component_subgraphs(G.to_undirected()), key=len)
+                number_of_nodes = nx.number_of_nodes(giant)
+                number_of_edges = nx.number_of_edges(giant)
+                average_clustering = nx.average_clustering(giant)
+                average_shortest_path_length = nx.average_shortest_path_length(giant)
+                average_degree = float(number_of_edges)/number_of_nodes
+                k[direction][i, j] = number_of_edges
+                C[direction][i, j] = average_clustering
+                L[direction][i, j] = average_shortest_path_length
+                D[direction][i, j] = average_degree
+                logging.info('Analyse Graph for thr_peak=%1.3f, thr_overlap=%1.3f' % (thr_peak, thr_overlap))
+
+    pickle.dump((k, C, L, D, factors, thresholds, directions),
+                 open('temp/struc_networkparameters_for_thr_peak_thr_overlap_hidens2018.p', 'wb'))
+
+
+# Final version
+
 
 
 
 if not os.path.isfile('temp/struc_networks_for_thr_peak_thr_overlap_hidens2018.p'): explore_parameter_space_for_structural_connectivity()
-
-
+if not os.path.isfile('temp/struc_networkparameters_for_thr_peak_thr_overlap_hidens2018.p'): analyse_structural_networks()
