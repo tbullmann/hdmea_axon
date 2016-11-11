686b80e832c079de27b02dd1f0c47d4f70c42869
tbullmann <bullmann.torsten@gmail.com>
2016-11-11 19:06:05 +0900

    Final pipeline to create figure 6.

--- publication/figure_06.py
+++ publication/figure_06.py
@@ -1,52 +1,15 @@
-import logging
+from hana.matlab import load_neurites, load_positions
+from hana.structure import find_overlap, all_overlaps
+from hana.plotting import plot_axon, plot_dendrite, plot_neuron_pair, plot_network, set_axis_hidens
 
-import numpy as np
 from matplotlib import pyplot as plt
 
-from hana.matlab import load_neurites, load_positions
-from hana.plotting import plot_axon, plot_dendrite, plot_neuron_pair
-from hana.structure import find_overlap, all_overlaps
-from hana.plotting import plot_network
-from hana.plotting import set_axis_hidens, plot_neuron_points, plot_neuron_id, highlight_connection
 
+import logging
 logging.basicConfig(level=logging.DEBUG)
 
 
-def test():
-    axon_delay, dendrite_peak = load_neurites ('data/hidens2018at35C_arbors.mat')
-    pos = load_positions('data/hidens_electrodes.mat')
-
-    print len(axon_delay)
-    print axon_delay.keys()
-
-    # neuron indicies are 6272, 9861, 7050, 5396, 3094, 6295, 7965, 6533, 3241, 3606, 2874, 5697, 10052, 8901, 10569,
-    # 7375, 10964, 5978, 7903, 9953, 7014, 6249, 4973, 5243, 8061]
-
-
-    pre = 3241
-    post =4973
-
-    ax = plt.subplot(111)
-
-    z = axon_delay[pre]
-    print np.nanmax(z)
-
-    plot_axon(ax, pos, z)
-
-    # for neuron in dendrite_peak:
-    #     z = dendrite_peak[neuron]
-    #     plot_dendrite(ax,pos,z)
-
-    z = dendrite_peak[post]
-    plot_dendrite(ax, pos, z)
-    plot_neuron_points(ax, axon_delay, pos)
-    plot_neuron_id(ax, axon_delay, pos)
-    highlight_connection(ax,(pre,post),pos)
-
-    set_axis_hidens(ax,pos)
-
-
-    plt.show()
+# Other plots, mainly used for finding good examples
 
 def test_plot_all_axonal_fields():
     """Display axonal field for each neuron, good examples are 3606, 9953, 3094"""
@@ -59,7 +22,8 @@ def test_plot_all_axonal_fields():
         ax.set_title ('axon for neuron %d' % neuron)
         plt.show()
 
-def test_plot_all_dendritic_fields(presynaptic_neuron):
+
+def test_plot_all_dendritic_fields_vs_one_axonal_field (presynaptic_neuron):
     """Display dendritic field for each neuron,
     good examples are 3606->7375,5243,4973,not 6272,6295,8061; 9953->not3606,7375,4973,5243;3094->3606, not7375,5243,4973"""
     axon_delay, dendrite_peak = load_neurites ('data/hidens2018at35C_arbors.mat')
@@ -72,24 +36,27 @@ def test_plot_all_dendritic_fields(presynaptic_neuron):
         ax.set_title ('axon for neuron %d, dendrite for neuron %d' % (presynaptic_neuron, postsynaptic_neuron))
         plt.show()
 
-def test_plot_overlap():
+
+# Early version
+
+def Figure06_only_overlap():
     """Plot overlap between axonal and dendritic fields of a presumably pre- and post-synaptic neuron pair"""
     axon_delay, dendrite_peak = load_neurites ('data/hidens2018at35C_arbors.mat')
     pos = load_positions('data/hidens_electrodes.mat')
     presynaptic_neuron = 3606
     postsynaptic_neuron = 4973  # 5243
-    delay = 2.3
-
+    delay = 2.3  # NOTE Arbitrary used for checking plot function
+    plt.figure('Figure 6', figsize=(16, 8))
     ax=plt.subplot(111)
-
     plot_neuron_pair(ax, pos, axon_delay, dendrite_peak, delay, postsynaptic_neuron, presynaptic_neuron)
-
     set_axis_hidens(ax,pos)
     ax.set_title ('neuron pair %d $\longrightarrow$ %d' % (presynaptic_neuron, postsynaptic_neuron))
     plt.show()
 
 
-def test_estimate_overlap():
+# Final version
+
+def Figure06():
     axon_delay, dendrite_peak = load_neurites ('data/hidens2018at35C_arbors.mat')
     pos = load_positions('data/hidens_electrodes.mat')
 
@@ -100,6 +67,7 @@ def test_estimate_overlap():
 
     ratio, delay = find_overlap(axon_delay, dendrite_peak, presynaptic_neuron, postsynaptic_neuron, thr_peak, thr_overlap)
 
+    plt.figure('Figure 6', figsize=(16, 8))
     ax1 = plt.subplot(121)
     plot_neuron_pair(ax1, pos, axon_delay, dendrite_peak, delay, postsynaptic_neuron, presynaptic_neuron)
     set_axis_hidens(ax1,pos)
@@ -113,9 +81,7 @@ def test_estimate_overlap():
     plt.show()
 
 
-# test()
-test_plot_all_axonal_fields()
-test_plot_all_dendritic_fields(3606)
-test_plot_overlap() # plots examplary overlap between axon and dendrite
-test_estimate_overlap()  # figure 6
-# figure10_prepare_data()
+# test_plot_all_axonal_fields()
+# test_plot_all_dendritic_fields_vs_one_axonal_field(3606)
+# Figure06_only_overlap
+Figure06()
