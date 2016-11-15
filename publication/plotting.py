f97d90f2d699d5a6bd26873b261acd0a5bdcfe4c
tbullmann <bullmann.torsten@gmail.com>
2016-11-15 16:37:32 +0900

    Initial working commit for plot_loglog_fit.

--- publication/plotting.py
+++ publication/plotting.py
@@ -1,10 +1,12 @@
 import numpy as np
+from scipy.optimize import curve_fit
 from matplotlib import pyplot as plt
 
 
 FIGURE_ARBORS_FILE = 'data/hidens2018at35C_arbors.mat'
 FIGURE_EVENTS_FILE = 'data/hidens2018at35C_events.mat'
 FIGURE_ELECTRODES_FILE = 'data/hidens_electrodes.mat'
+
 
 def plot_parameter_dependency(ax, Z, x, y, w=None, levels=None, fmt='%d'):
     """Plotting parameter dependency"""
@@ -27,3 +29,32 @@ def plot_parameter_dependency(ax, Z, x, y, w=None, levels=None, fmt='%d'):
         plt.legend(loc=2, handles=[black_patch, red_patch])
 
 
+def plot_loglog_fit(ax, y, title = 'size distribution', datalabel = 'data', xlabel = 'rank', ylabel = 'size'):
+
+    # sort y data in descending order, define corresponding rank starting from 1 for larges y
+    y.sort(reverse=True)
+    x = range(1, len(y) + 1)
+
+    # Define domain for the fitted curves
+    min_exp = 0
+    max_exp = np.ceil(np.log10(len(y)))
+    fitted_x = np.logspace(min_exp, max_exp, base=10)
+
+    # Define fitted curve
+    def f(x, a, b): return a * np.power(x, b)
+
+    # Fit curve to data and predict y
+    popt, pcov = curve_fit(f, x, y)
+    fitted_y = f(fitted_x, *popt)
+
+    # Plot data and legend
+    ax.plot(x, y, '.k', label=datalabel)
+    ax.plot(fitted_x, fitted_y, 'r-', label="fit $y=%.3f x ^{%.3f}$" % (popt[0], popt[1]))
+    plt.legend(loc='upper right')
+
+    # Scale and label axes, add title
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
