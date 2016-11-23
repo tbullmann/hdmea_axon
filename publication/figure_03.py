34c7111c15fd26a77c21dd40a41fc1f9742c9d62
tbullmann <bullmann.torsten@gmail.com>
2016-11-23 17:42:50 +0900

    Moved subplot to figure 2

---
+++ publication/figure_03.py
@@ -0,0 +1,18 @@
+import logging
+
+from matplotlib import pyplot as plt
+
+from publication.figure_02 import testing
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Testing code
+
+# Final figure 3
+
+def figure03():
+    plt.figure('Figure 3', figsize=(12,9))
+
+
+
+
