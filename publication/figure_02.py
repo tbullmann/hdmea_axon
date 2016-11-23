342d756310ffe95becfa80109ff594c1466e862a
tbullmann <bullmann.torsten@gmail.com>
2016-11-23 18:35:05 +0900

    Final extended figure 2, working.

--- publication/figure_02.py
+++ publication/figure_02.py
@@ -150,12 +150,20 @@ def figure02():
     positive_delay = mean_delay > 0
     axon = np.multiply(positive_delay, valid_delay)
 
+    # Define examples
+    background_color = 'green'
+    index_background_example = 500
+    indicies_background = neighborhood(neighbors, index_background_example)
+    foreground_color = 'blue'
+    index_foreground_example = 8624
+    indicies_foreground = neighborhood(neighbors, index_foreground_example)
+
     # -------------- first row
 
     # subplot original unaligned traces
     ax1 = plt.subplot(331)
     ax1.plot(t, V.T,'-', color='gray', label='all' )
-    ax1.plot(t, V_AIS, 'k-', label='AIS')
+    ax1.plot(t, V_AIS, 'r-', label='AIS')
     ax1.scatter(t_min[index_AIS], -550, marker='^', s=100, edgecolor='None', facecolor='red')
     # annotate_x_bar(peak_peak_domain(t, V_AIS), min(V_AIS)/2, text=' $\delta_p$ = %0.3f ms' % peak_peak_width(t, V_AIS))
     legend_without_multiple_labels(ax1, loc=4, frameon=False)
@@ -170,17 +178,17 @@ def figure02():
     h1 = ax2.scatter(x, y, c=t_min, s=10, marker='o', edgecolor='None', cmap='gray')
     h2 = plt.colorbar(h1)
     h2.set_label(r'$\tau$ [ms]')
-    cross_hair(ax2, x_AIS, y_AIS)
+    add_AIS_and_example_neighborhoods(ax2, x, y, index_AIS, indicies_background, indicies_foreground)
     set_axis_hidens(ax2, pos)
     label_subplot(ax2, 'B', xoffset=-0.015, yoffset=-0.01)
 
-
     # subplot histogram of delays
     ax3 = plt.subplot(333)
-    ax3.hist(t_min, bins=len(t), facecolor='gray', edgecolor='gray')
-    ax3.scatter(t_min[index_AIS], 120, marker='v', s=100, edgecolor='None', facecolor='red')
+    ax3.hist(t_min, bins=len(t), facecolor='gray', edgecolor='gray', label='measured')
+    ax3.scatter(t_min[index_AIS], 10, marker='v', s=100, edgecolor='None', facecolor='red', zorder=10)
     # ax3.vlines(0, 0, 180, color='k', linestyles=':')
-    ax3.hlines(len(x)/len(t), min(t), max(t), color='k', linestyles='--')
+    ax3.hlines(len(x)/len(t), min(t), max(t), color='k', linestyles='--', label='uniform')
+    ax3.legend(frameon=False)
     ax3.set_ylim((0,200))
     ax3.set_xlim((min(t),max(t)))
     ax3.set_ylabel(r'count')
@@ -190,24 +198,17 @@ def figure02():
 
     # ------------- second row
 
-    background_color = 'green'
-    example_background_index = 500
-    background_indicies = neighborhood(neighbors, example_background_index)
-    foreground_color = 'blue'
-    example_foreground_index = 8624
-    foreground_indicies = neighborhood(neighbors, example_foreground_index)
-
     # Subplot neighborhood with uncorrelated negative peaks
     ax4 = plt.subplot(637)
-    plot_traces_and_delays(ax4, V, t, t_min, background_indicies, offset=-2, ylim=(-10, 5), color=background_color)
-    ax4.text(-3.5, -7.5, r'$s_{\tau}$ = %0.3f ms' % std_delay[example_background_index])
+    plot_traces_and_delays(ax4, V, t, t_min, indicies_background, offset=-2, ylim=(-10, 5), color=background_color)
+    ax4.text(-3.5, -7.5, r'$s_{\tau}$ = %0.3f ms' % std_delay[index_background_example])
     ax4.set_yticks([-10,-5,0,5])
     label_subplot(ax4, 'D', xoffset=-0.035, yoffset=-0.01)
 
     # Subplot neighborhood with correlated negative peaks
     ax5 = fig.add_subplot(6, 3, 10)
-    plot_traces_and_delays(ax5, V, t, t_min, foreground_indicies, offset=-20, ylim=(-30, 10), color=foreground_color)
-    ax5.text(-3.5, -22, r'$s_{\tau}$ = %0.3f ms' % std_delay[example_foreground_index])
+    plot_traces_and_delays(ax5, V, t, t_min, indicies_foreground, offset=-20, ylim=(-30, 10), color=foreground_color)
+    ax5.text(-3.5, -22, r'$s_{\tau}$ = %0.3f ms' % std_delay[index_foreground_example])
     ax5.set_yticks([-30,-20,-10,0,10])
     label_subplot(ax5, 'F', xoffset=-0.035, yoffset=-0.01)
 
@@ -216,18 +217,22 @@ def figure02():
     h1 = ax6.scatter(x, y, c=std_delay, s=10, marker='o', edgecolor='None', cmap='gray')
     h2 = plt.colorbar(h1)
     h2.set_label(r'$s_{\tau}$ [ms]')
-    cross_hair(ax6, x_AIS, y_AIS)
-    ax6.scatter(x[background_indicies], y[background_indicies], s=10, marker='o', edgecolor='None', facecolor='green')
-    ax6.scatter(x[foreground_indicies], y[foreground_indicies], s=10, marker='o', edgecolor='None', facecolor='blue')
+    add_AIS_and_example_neighborhoods(ax6, x, y, index_AIS, indicies_background, indicies_foreground)
     set_axis_hidens(ax6, pos)
     label_subplot(ax6, 'F', xoffset=-0.015, yoffset=-0.01)
 
     # subplot std_delay histogram
     ax7 = plt.subplot(336)
-    ax7.hist(std_delay, bins=np.arange(0, max(t_min), step=DELAY_EPSILON), facecolor='gray', edgecolor='gray')
-    ax7.hist(std_delay, bins=np.arange(0, thr, step=DELAY_EPSILON), facecolor='k', edgecolor='k')
+    ax7.hist(std_delay, bins=np.arange(0, max(t_min), step=DELAY_EPSILON), facecolor='gray', edgecolor='gray', label='background')
+    ax7.hist(std_delay, bins=np.arange(0, thr, step=DELAY_EPSILON), facecolor='k', edgecolor='k', label='foreground')
+    ax7.scatter(std_delay[index_foreground_example], 25, marker='v', s=100, edgecolor='None', facecolor=foreground_color, zorder=10)
+    ax7.scatter(std_delay[index_background_example], 25, marker='v', s=100, edgecolor='None', facecolor=background_color, zorder=10)
+    ax7.scatter(expected_std_delay, 25, marker='v', s=100, edgecolor='black', facecolor='gray', zorder=10)
+    ax7.text(expected_std_delay, 30, r'$\frac{8}{\sqrt{12}}$ ms',  horizontalalignment='center', verticalalignment='bottom', zorder=10)
+    ax7.legend(frameon=False)
     ax7.vlines(0, 0, 180, color='k', linestyles=':')
     # ax7.hlines(len(x) / len(t), min(t), max(t), color='k', linestyles='--')
+    ax7.set_ylim((0, 600))
     ax7.set_xlim((0, 4))
     ax7.set_ylabel(r'count')
     ax7.set_xlabel(r'$s_{\tau}$ [ms]')
@@ -239,26 +244,35 @@ def figure02():
     # plot map of delay greater zero
     ax8 = plt.subplot(337)
     ax8.scatter(x, y, c=positive_delay, s=10, marker='o', edgecolor='None', cmap='gray_r')
-    cross_hair(ax8, x_AIS, y_AIS)
+    add_AIS_and_example_neighborhoods(ax8, x, y, index_AIS, indicies_background, indicies_foreground)
+    ax8.text(300, 300, r'$\tau > \tau_{AIS}$', bbox=dict(facecolor='white', pad=5, edgecolor='none'))
     set_axis_hidens(ax8, pos)
     label_subplot(ax8, 'H', xoffset=-0.005, yoffset=-0.01)
 
     # plot map of delay greater zero
     ax9 = plt.subplot(338)
     ax9.scatter(x, y, c=valid_delay, s=10, marker='o', edgecolor='None', cmap='gray_r')
-    cross_hair(ax9, x_AIS, y_AIS)
+    ax9.text(300, 300, r'$s_{\tau} > s_{min}$', bbox=dict(facecolor='white', pad=5, edgecolor='none'))
+    add_AIS_and_example_neighborhoods(ax9, x, y, index_AIS, indicies_background, indicies_foreground)
     set_axis_hidens(ax9, pos)
     label_subplot(ax9, 'I', xoffset=-0.005, yoffset=-0.01)
 
     # plot map of delay greater zero
     ax10 = plt.subplot(339)
     ax10.scatter(x, y, c=axon, s=10, marker='o', edgecolor='None', cmap='gray_r')
-    cross_hair(ax10, x_AIS, y_AIS)
+    ax10.text(300, 300, 'axon', bbox=dict(facecolor='white', pad=5, edgecolor='none'))
+    add_AIS_and_example_neighborhoods(ax10, x, y, index_AIS, indicies_background, indicies_foreground)
     set_axis_hidens(ax10, pos)
     label_subplot(ax10, 'J', xoffset=-0.005, yoffset=-0.01)
 
 
     plt.show()
+
+
+def add_AIS_and_example_neighborhoods(ax6, x, y, index_AIS, indicies_background, indicies_foreground):
+    cross_hair(ax6, x[index_AIS], y[index_AIS])
+    ax6.scatter(x[indicies_background], y[indicies_background], s=10, marker='o', edgecolor='None', facecolor='green')
+    ax6.scatter(x[indicies_foreground], y[indicies_foreground], s=10, marker='o', edgecolor='None', facecolor='blue')
 
 
 # testing_load_traces()
