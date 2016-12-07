46d9966e05691e2dd245ca3acbbc73bb5f012b29
tbullmann <bullmann.torsten@gmail.com>
2016-12-07 18:45:55 +0900

    Added Labels

--- publication/figure_08.py
+++ publication/figure_08.py
@@ -8,7 +8,7 @@ from hana.polychronous import filter, combine, group, plot, plot_pcg_on_network,
 from hana.recording import load_positions, load_timeseries, HIDENS_ELECTRODES_FILE, partial_timeseries
 from hana.segmentation import load_compartments, load_neurites, neuron_position_from_trigger_electrode
 from hana.structure import all_overlaps
-from publication.plotting import FIGURE_ARBORS_FILE, plot_loglog_fit, FIGURE_EVENTS_FILE
+from publication.plotting import FIGURE_ARBORS_FILE, plot_loglog_fit, FIGURE_EVENTS_FILE, label_subplot, shrink_axes
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -57,7 +57,31 @@ def figure08():
         connected_events = filter(timeseries, all_delays)
         pickle.dump(connected_events, open('temp/connected_events.p', 'wb'))
 
+    if not os.path.isfile('temp/connected_surrogate_events.p'):
+        print ('DO!')
+        all_delays = pickle.load(open('temp/all_delays.p', 'rb'))
+        timeseries = pickle.load(open('temp/partial_timeseries.p', 'rb'))
+
+        def random_shuffle(a):
+            keys = a.keys()
+            # np.random.shuffle(keys)
+            print keys
+            pre, post = zip(*keys)
+            new_post = list(post)
+            np.random.shuffle(new_post)
+            keys = zip(pre,new_post)
+            print keys
+            b = dict(zip(keys, a.values()))
+            return b
+
+        surrogate_delays = random_shuffle(all_delays)
+        connected_events = filter(timeseries, surrogate_delays)
+        pickle.dump(connected_events, open('temp/connected_surrogate_events.p', 'wb'))
+
+
     connected_events = pickle.load(open('temp/connected_events.p', 'rb'))
+    connected_surrogate_events = pickle.load(open('temp/connected_surrogate_events.p', 'rb'))
+    # TODO: Add plot of surrogate polychronous groups
     graph_of_connected_events = combine(connected_events)
     list_of_polychronous_groups = group(graph_of_connected_events)
 
@@ -68,25 +92,31 @@ def figure08():
 
 
     # Plotting
-    plt.figure('Figure 8', figsize=(12,12))
-
+    fig = plt.figure('Figure 8', figsize=(12, 12))
+    fig.suptitle('Figure 7. Polychronous groups', fontsize=14, fontweight='bold')
 
     # plot example of a single polychronous group
-    plt.subplot(221)
+    ax1 = plt.subplot(221)
     plot(graph_of_connected_events)
     plt.xlim((590,700))
     plt.ylim((0,60))
+    shrink_axes(ax1, xshrink=0.01)
+    label_subplot(ax1, 'A', xoffset=-0.05, yoffset=-0.01)
 
     # plot size distribution
     ax2 = plt.subplot(222)
     polychronous_group_size = list(len(g) for g in list_of_polychronous_groups)
-    plot_loglog_fit(ax2, polychronous_group_size)
+    plot_loglog_fit(ax2, polychronous_group_size, rank_threshold=100)  #exclude 100 largest groups from the fit
+
+    label_subplot(ax2, 'B', xoffset=-0.06, yoffset=-0.01)
 
     # plot example of a single polychronous group with arrows
-    ax = plt.subplot(223)
+    ax3 = plt.subplot(223)
     polychronous_group = list_of_polychronous_groups[3]   #10 too complex, 7 highly repetetive
-    plot_pcg(ax, polychronous_group)
+    plot_pcg(ax3, polychronous_group)
     plt.ylim((0,60))
+    shrink_axes(ax3, xshrink=0.01)
+    label_subplot(ax3, 'C', xoffset=-0.05, yoffset=-0.01)
 
 
     # plot example of a single polychronous group onto network
@@ -97,6 +127,7 @@ def figure08():
     all_delay = pickle.load(open('temp/all_delays.p', 'rb'))
     plot_pcg_on_network(ax4, polychronous_group, all_delay, neuron_pos)
     ax4.set_title('network activation by polychronous group')
+    label_subplot(ax4, 'D', xoffset=-0.04, yoffset=-0.01)
 
 
     # # plot polychronous groups with in different arrow colors
