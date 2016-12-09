a0d19d277740f4189ad399f611ecddb31bfe1301
tbullmann <bullmann.torsten@gmail.com>
2016-12-09 20:41:19 +0900

    First commit of pipeline to make figure S02

---
+++ publication/figure_S02.py
@@ -0,0 +1,50 @@
+import pickle
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+from publication.plotting import plot_parameter_dependency, TEMPORARY_PICKELED_NETWORKS, compare_networks, \
+    format_parameter_plot
+
+
+def figure02(networks_pickel_name):
+
+    structural_strengths, structural_delays, functional_strengths, functional_delays \
+        = pickle.load( open(networks_pickel_name, 'rb'))
+
+    # Making figure
+    fig = plt.figure('Figure S2', figsize=(16, 16))
+    fig.suptitle('Figure S2. Compare structural and functional connectivity', fontsize=14, fontweight='bold')
+
+    structural_thresholds, functional_thresholds, intersection_size, structural_index, jaccard_index, functional_index\
+        = compare_networks(structural_strengths, functional_strengths, scale='log')
+
+    ax1 = plt.subplot(2,2,1)
+    plot_parameter_dependency(ax1, intersection_size, structural_thresholds, functional_thresholds,
+                              fmt='%d', levels=(1,2,5,10,20,50,100,150))
+    format_parameter_plot(ax1)
+    ax1.set_title(r'intersection size $|F \cup S|$')
+
+    ax2 = plt.subplot(2,2,2)
+    plot_parameter_dependency(ax2, structural_index, structural_thresholds, functional_thresholds,
+                              fmt='%1.1f', levels=np.linspace(0,1,11))
+    format_parameter_plot(ax2)
+    ax2.set_title(r'Structural index ${|F \cup S|}/{|S|}$')
+
+    ax3 = plt.subplot(2,2,3)
+    plot_parameter_dependency(ax3, jaccard_index, structural_thresholds, functional_thresholds,
+                              fmt='%1.1f', levels=np.linspace(0,1,11))
+    format_parameter_plot(ax3)
+    ax3.set_title(r'Jaccard index ${|F \cup S|}/{|F \cap S|}$')
+
+    ax4 = plt.subplot(2,2,4)
+    plot_parameter_dependency(ax4, functional_index, structural_thresholds, functional_thresholds,
+                              fmt='%1.1f', levels=np.linspace(0,1,11))
+    format_parameter_plot(ax4)
+    ax4.set_title(r'Functional index ${|F \cup S|}/{|F|}$')
+
+
+    plt.show()
+
+
+figure02(TEMPORARY_PICKELED_NETWORKS)
