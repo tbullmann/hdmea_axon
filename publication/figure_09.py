4c8971904faeacb1ffb2041a0a011b942195ad45
tbullmann <bullmann.torsten@gmail.com>
2017-02-09 16:34:19 +0900

    Important conversion of an dictionary to an array of values perserving order in the keys (array) Final figure 9 with 2 panels

--- publication/figure_09.py
+++ publication/figure_09.py
@@ -14,7 +14,7 @@ from hana.recording import load_positions
 from hana.segmentation import neuron_position_from_trigger_electrode, load_compartments
 from hana.plotting import plot_neuron_points, plot_neuron_id, plot_network, set_axis_hidens
 from publication.plotting import FIGURE_ARBORS_FILE, TEMPORARY_PICKELED_NETWORKS, compare_networks, \
-    label_subplot, __correlate_two_dicts, kernel_density
+    label_subplot, __correlate_two_dicts, kernel_density, axes_to_3_axes
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -23,17 +23,14 @@ def figure09 (networks_pickel_name):
     structural_strengths, structural_delays, functional_strengths, functional_delays = pickle.load( open(networks_pickel_name, 'rb'))
 
     # Making figure
-    fig = plt.figure('Figure 9', figsize=(16, 8))
+    fig = plt.figure('Figure 9', figsize=(13, 6))
     fig.suptitle('Figure 9. Putative chemical synaptic connections', fontsize=14, fontweight='bold')
 
     structural_thresholds, functional_thresholds, intersection_size, structural_index, jaccard_index, functional_index\
         = compare_networks(structural_strengths, functional_strengths, scale='log')
 
     ax1 = plt.subplot(121)
-    label_subplot(ax1,'A', xoffset=-0.04,yoffset=-0.02)
-    pairs = plot_synapse_delays(ax1, structural_delays, functional_delays, functional_strengths, ylim=(-2,5))
-    ax1.set_xlabel (r'score $z_{max}$')
-    ax1.set_ylabel (r'estimated synaptic delay $\tau_{synapse}$ [ms]')
+    delayed_pairs, simultaneous_pairs, delay_synapse = plot_synapse_delays(ax1, structural_delays, functional_delays, functional_strengths, ylim=(-2,7))
 
     ax2 = plt.subplot(122)
     trigger, _, _, _ = load_compartments(FIGURE_ARBORS_FILE)
@@ -41,45 +38,74 @@ def figure09 (networks_pickel_name):
     neuron_pos = neuron_position_from_trigger_electrode(pos, trigger)
     plot_neuron_points(ax2, unique_neurons(structural_delays), neuron_pos)
     plot_neuron_id(ax2, trigger, neuron_pos)
-    plot_network(ax2, structural_delays, neuron_pos, color='gray')
-    plot_network(ax2, pairs, neuron_pos, color='black')
-
+    # plot_network(ax2, structural_delays, neuron_pos, color='gray')
+    plot_network(ax2, simultaneous_pairs, neuron_pos, color='red')
+    plot_network(ax2, delayed_pairs, neuron_pos, color='green')
     # Legend by proxy
-    ax2.hlines(0,0,0,linestyle='-',color='gray',label='remaining')
-    ax2.hlines(0,0,0,linestyle='-',color='black',label='putative chemical')
-    ax2.text(200,150,r'$\rho=$1 electrode')
-
+    ax2.hlines(0,0,0,linestyle='-',color='red',label='<1ms')
+    ax2.hlines(0,0,0,linestyle='-',color='green',label='>1ms')
+    ax2.text(200,200,r'$\mathsf{\rho=300\mu m^2}$', fontsize=14)
     plt.legend(frameon=False)
-
     set_axis_hidens(ax2)
+
+    # Label subplots
+    label_subplot(ax1,'A', xoffset=-0.04,yoffset=-0.02)
     label_subplot(ax2,'B',xoffset=-0.04,yoffset=-0.02)
 
     plt.show()
 
 
-def plot_synapse_delays(axScatter, structural_delay, functional_delay, functional_strength, xlim=None, ylim=None, scaling ='count'):
+def plot_synapse_delays(ax, structural_delay, functional_delay, functional_strength, xlim=None, ylim=None, xscaling='log', yscaling ='count', naxes=3):
     """Plot corretion and marginals"""
+    # New axes
+    if naxes==2:
+        axScatter=ax
+        fig = ax.get_figure()
+        divider = make_axes_locatable(axScatter)
+        axHisty = divider.new_horizontal(size="50%", pad=0.05)
+        fig.add_axes(axHisty)
+    if naxes==3:
+        rect_histx, rect_histy, rect_scatter = axes_to_3_axes(ax)
+        axScatter = plt.axes(rect_scatter)
+        axHistx = plt.axes(rect_histx)
+        axHisty = plt.axes(rect_histy)
+
     # getting the data
     delay_axon, timing_spike, pairs = __correlate_two_dicts(structural_delay, functional_delay)
     delay_synapse = timing_spike - delay_axon
     __, strength_synapse, __ = __correlate_two_dicts(structural_delay, functional_strength)
+    print strength_synapse
+    strength_synapse = dict2array_like_index(functional_strength, pairs)
+    print strength_synapse
 
-    # Find putative chemical synapse with synaptic delay > 1ms
-    indices = np.where(delay_synapse>1)
-    synaptic_pairs = np.array(pairs)[indices]
+    # Find putative chemical synapse with synaptic delay > 1ms, and other with delays <= 1ms
+    delayed_indices = np.where(delay_synapse>1)
+    delayed_pairs = np.array(pairs)[delayed_indices]
+    simultaneous_indices = np.where(delay_synapse<=1)
+    simultaneous_pairs = np.array(pairs)[simultaneous_indices]
 
     # scatter plot
-    axScatter.scatter(strength_synapse, delay_synapse, color='gray', label='remaining')
-    axScatter.scatter(strength_synapse[indices],delay_synapse[indices],color='black', label='putative chemical')
-    axScatter.set_xscale('log')
-    plt.legend(frameon=False)
+    axScatter.scatter(strength_synapse, delay_synapse, color='red', label='<1ms')
+    axScatter.scatter(strength_synapse[delayed_indices],delay_synapse[delayed_indices], color='green', label='>1ms')
+    axScatter.set_xscale(xscaling)
+    axScatter.legend(frameon=False, scatterpoints=1)
+    axScatter.set_xlabel(r'$\mathsf{z_{max}}$', fontsize=14)
+    axScatter.set_ylabel(r'$\mathsf{\tau_{synapse}\ [ms]}$', fontsize=14)
 
     # density plot
-    divider = make_axes_locatable(axScatter)
-    axHisty = divider.new_horizontal(size="50%", pad=0.05)
-    fig = axScatter.get_figure()
-    fig.add_axes(axHisty)
-    kernel_density(axHisty, delay_synapse, scaling=scaling, style='k-', orientation='horizontal')
+    kernel_density(axHisty, delay_synapse, scaling=yscaling, style='k-', orientation='horizontal')
+    if naxes==3:
+        # joint legend by proxies
+        plt.sca(ax)
+        plt.vlines(0, 0, 0, colors='green', linestyles='-', label='>1ms')
+        plt.vlines(0, 0, 0, colors='red', linestyles='-', label='<1ms')
+        plt.vlines(0, 0, 0, colors='black', linestyles='-', label='all')
+        plt.legend(frameon=False, fontsize=12)
+
+        # kernel_density(axHistx, strength_synapse, scaling=yscaling, style='k-', orientation='vertical')
+        kernel_density(axHistx, strength_synapse[delayed_indices], scaling=yscaling, style='g-', orientation='vertical')
+        kernel_density(axHistx, strength_synapse[simultaneous_indices], scaling=yscaling, style='r-', orientation='vertical')
+        axHistx.set_xscale(xscaling)
 
     # define limits
     max_strength_synapse = max(strength_synapse)
@@ -89,6 +115,7 @@ def plot_synapse_delays(axScatter, structural_delay, functional_delay, functiona
     # set limits
     axScatter.set_xlim(xlim)
     axScatter.set_ylim(ylim)
+    axHistx.set_xlim(axScatter.get_xlim())
     axHisty.set_ylim(axScatter.get_ylim())
 
     # add hlines to Scatter
@@ -96,13 +123,30 @@ def plot_synapse_delays(axScatter, structural_delay, functional_delay, functiona
     axScatter.hlines(-1, 0, max_strength_synapse*2, linestyles=':')
     axScatter.hlines(+1, 0, max_strength_synapse*2, linestyles=':')
 
-
     # no labels
     nullfmt = NullFormatter()  # no labels
-
+    axHistx.xaxis.set_major_formatter(nullfmt)
+    axHistx.yaxis.set_major_formatter(nullfmt)
     axHisty.xaxis.set_major_formatter(nullfmt)
     axHisty.yaxis.set_major_formatter(nullfmt)
 
-    return synaptic_pairs
+    return delayed_pairs, simultaneous_pairs, delay_synapse
+
+
+def dict2array_like_index(dictionary, keys):
+    """
+    Returns an array with elements from the dictionary preserving the order of keys.
+    :param dictionary:
+    :param keys:
+    :return:
+    """
+    strength_synapse = []
+    for key in keys:
+        value = dictionary[key]
+        # print key, value
+        strength_synapse.append(value)
+    strength_synapse = np.array(strength_synapse)
+    return strength_synapse
+
 
 figure09(TEMPORARY_PICKELED_NETWORKS)
