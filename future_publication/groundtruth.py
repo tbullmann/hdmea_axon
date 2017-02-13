a96e0f84cae11573ae402abc8063d44402d4fd36
tbullmann <bullmann.torsten@gmail.com>
2017-02-13 18:29:51 +0900

    Messing around with colorbars

--- future_publication/groundtruth.py
+++ future_publication/groundtruth.py
@@ -48,20 +48,37 @@ def testing_load_traces(neuron):
     fig = plt.figure('Figure X neuron %d' % neuron, figsize=(13, 7))
     fig.suptitle('Figure X. Ground truth for neuron %d' % neuron, fontsize=14, fontweight='bold')
 
-    V2size = lambda x : np.sqrt(np.abs(x))/max(np.sqrt(np.abs(x))) * 100
+    max_V = np.round(np.max(np.abs(x)),-3)
+    print max_V
+    print max(delay)
+    V2size = lambda x : np.sqrt(np.abs(x))/np.sqrt(max_V) * 100
 
     # Map axons
-    ax4 = plt.subplot(121)
+    ax4 = plt.subplot(131)
     plot_image_axon_delay_voltage(ax4, path, axon_old, delay_old, V, x, y, transform=V2size)
     cross_hair(ax4, x_AIS, y_AIS, color='red')
     set_axis_hidens(ax4)
     ax4.set_title('Method I')
 
-    ax5 = plt.subplot(122)
-    plot_image_axon_delay_voltage(ax5, path, axon, delay, V, x, y, transform=V2size)
+    ax5 = plt.subplot(132)
+    ax5h = plot_image_axon_delay_voltage(ax5, path, axon, delay, V, x, y, transform=V2size)
     cross_hair(ax5, x_AIS, y_AIS, color='red')
     set_axis_hidens(ax5)
     ax5.set_title('Method II')
+
+    # from mpl_toolkits.axes_grid1 import make_axes_locatable
+    # divider = make_axes_locatable(ax5)
+    # cax1 = divider.append_axes("right", size="5%", pad=0.05)
+    cbar = fig.colorbar(ax5h)
+    cbar.set_label(r'$\mathsf{\tau_{axon}\ [ms]}$', fontsize=14)
+
+    for V in [1, 10, 100, 1000]:
+        plt.scatter([],[],s=V2size(V), color='white', edgecolor='black', label='%d' % V)
+    # leg = plt.legend(scatterpoints=1, frameon=False, loc = 'upper left', bbox_to_anchor = (1, 1),  title = r'$\mathsf{V_n\ [\mu V]}$')
+    leg = plt.legend(scatterpoints=1, frameon=False, title = r'$\mathsf{V_n\ [\mu V]}\ \ \ \ \ $')
+    leg.get_title().set_fontsize(14)
+
+
 
     plt.show()
 
@@ -69,7 +86,8 @@ def testing_load_traces(neuron):
 def plot_image_axon_delay_voltage(ax, path, axon, delay, V, x, y, transform=np.abs):
     plot_neuron_image(path)
     radius = transform(V) if transform else V
-    ax.scatter(x[axon], y[axon], s=radius[axon], c=delay[axon], marker='o', alpha=0.5)
+    s = ax.scatter(x[axon], y[axon], s=radius[axon], c=delay[axon], marker='o', alpha=0.5)
+    return s
 
 
 def neighbors_from_electrode_positions(x, y, neighborhood_radius = 20):
@@ -100,10 +118,26 @@ def plot_neuron_image(path, transform=np.sqrt, cmap='gray_r'):
         # NOTE: yend and ystart are reverted due to hidens coordinate system TODO: Maybe fix
 
 
+def distanceBetweenCurves(C1, C2):
+    """
+    From http://stackoverflow.com/questions/13692801/distance-matrix-of-curves-in-python
+    Note: According to https://en.wikipedia.org/wiki/Hausdorff_distance this is defined
+    as max(H1, H2) and not (H1 + H2) / 2.
+    :param C1:
+    :param C2:
+    :return:
+    """
+    D = scipy.spatial.distance.cdist(C1, C2, 'euclidean')
+
+    #none symmetric Hausdorff distances
+    H1 = np.max(np.min(D, axis=1))
+    H2 = np.max(np.min(D, axis=0))
+
+    return (H1 + H2) / 2.
 
 
 
 testing_load_traces(1544)
-testing_load_traces(1536)
+# testing_load_traces(1536)
 
 
