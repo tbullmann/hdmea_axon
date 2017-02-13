0ebbec4fbdc7acad7a4c5d7fe804b8d0dfc7936c
tbullmann <bullmann.torsten@gmail.com>
2017-02-13 20:39:41 +0900

    test_flows working

--- future_publication/method3.py
+++ future_publication/method3.py
@@ -16,36 +16,39 @@ def test_flows(neuron=5, coarse=True):
     V, t, x, y, trigger, neuron = load_traces(filename)
 
     t *= 1000  # convert to ms
-    time = -4  # ms
+    timebefore = -4  # ms
+    timeafter = 1  # ms
 
+    gxB, gyB, gVB, gVsuccB, gvxB, gvyB, velocityB = velocity_by_LK(V, t, x, y, timebefore, coarse=coarse)
+    gx, gy, gV, gVsucc, gvx, gvy, velocity = velocity_by_LK(V, t, x, y, timeafter, coarse=coarse)
+
+    ax1 = plt.subplot(121)
+    plt.quiver(gxB, gyB, gvxB, gvyB, color='blue')
+    plt.quiver(gx, gy, gvx, gvy, color='red')
+    set_axis_hidens(ax1)
+
+    ax2 = plt.subplot(122)
+    plt.hist(np.ravel(velocityB), bins=200, edgecolor='None', color='blue', alpha=0.5)
+    plt.hist(np.ravel(velocity), bins=200, edgecolor='None', color='red', alpha=0.5)
+
+    plt.show()
+
+
+def velocity_by_LK(V, t, x, y, time, coarse=True):
     tspacing = np.abs((np.median(np.diff(t))))
     xspacing = np.abs((np.median(np.diff(x))))/2
     yspacing = np.abs((np.median(np.diff(y))))
     if coarse:
         xspacing, yspacing = 2 * xspacing, 2 * yspacing
+    logging.info('Scale = (dx,dy,dt) = (%.3f um, %.3f um, %.3f ms)' % (xspacing, yspacing, tspacing))
 
-    logging.info('Scale = (dx,dy,dt) = (%.3f um, %.3f um, %.3f ms)' %  (xspacing, yspacing, tspacing) )
-
-    grid_x, grid_y, grid_V1 = interpolate(x, y, np.ravel(V[:, t==time]), xspacing=xspacing, yspacing=yspacing)
-    _,_,grid_V2 = interpolate(x, y, np.ravel(V[:, t==time+0.05]), xspacing=xspacing, yspacing=yspacing)
-
-    logging.info('Interpolation to %d points' % (np.shape(grid_x)[0]*np.shape(grid_x)[1]))
-
+    index = np.where(t == time)[0]
+    grid_x, grid_y, grid_V1 = interpolate(x, y, np.ravel(V[:, index]), xspacing=xspacing, yspacing=yspacing)
+    _, _, grid_V2 = interpolate(x, y, np.ravel(V[:, index + 1]), xspacing=xspacing, yspacing=yspacing)
+    logging.info('Interpolation to %d points' % (np.shape(grid_x)[0] * np.shape(grid_x)[1]))
     xdot, ydot = LucasKanade(grid_V1, grid_V2)
-    # xdot, ydot = HS(grid_V1,grid_V2)
-
-    velocity = np.sqrt(xdot*xdot + ydot*ydot)
-
-    ax1 = plt.subplot(121)
-    plt.contour(grid_x, grid_y, -grid_V1, colors=('red',), levels=(5,))
-    plt.contour(grid_x, grid_y, -grid_V2, colors=('green',), levels=(5,))
-    plt.quiver(grid_x,grid_y, xdot, ydot)
-    set_axis_hidens(ax1)
-
-    ax2 = plt.subplot(222)
-    plt.hist(np.ravel(velocity), bins=200)
-
-    plt.show()
+    velocity = np.sqrt(xdot * xdot + ydot * ydot)
+    return grid_x, grid_y, grid_V1, grid_V2, xdot, ydot, velocity
 
 
 test_flows()
