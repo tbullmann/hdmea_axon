c55abf4c298f1c364f8914636821ba758f1b19be
tbullmann <bullmann.torsten@gmail.com>
2017-02-14 15:17:05 +0900

    cartesian x y to hexagonal grid i j, with i j at 0 0

--- future_publication/grid.py
+++ future_publication/grid.py
@@ -11,12 +11,21 @@ from publication.plotting import FIGURE_NEURON_FILE_FORMAT
 logging.basicConfig(level=logging.DEBUG)
 
 
-class affine_transform:
+class AffineTransformation:
     """
     T_AB dot A = B
+
+    T_AB = A dot B^-1
+    T_AB = T_AB^-1
+
+    B = T_AB dot A
+    A = T_BA dot B
     """
 
     def __init__(self, coordinates_A, coordinates_B):
+        self.match(coordinates_A, coordinates_B)
+
+    def match(self, coordinates_A, coordinates_B):
         ones = np.ones((1, np.shape(coordinates_A)[1]))
         B = np.vstack((coordinates_B, ones))
         A = np.vstack((coordinates_A, ones))
@@ -34,28 +43,51 @@ class affine_transform:
     @staticmethod
     def transform(coordinates_A, T):
         # type: (np.ndarray, np.ndarray) -> np.ndarray
-        ones = np.ones((1, np.shape(coordinates_A)[1]))
+        if coordinates_A.ndim==2:
+            n_coordinates = np.shape(coordinates_A)[1]
+        else:  # there was only one point, make its coordinates a 2 dimensional row vector
+            n_coordinates = 1
+            coordinates_A = np.atleast_2d(coordinates_A).T
+
+        ones = np.ones((1, n_coordinates))
         A = np.vstack((coordinates_A, ones))
         B = np.dot(T, A)
         coordinates_B = B[:2, :]
         return coordinates_B
 
 
-class transform_hidens_grid(affine_transform):
-    def __init__(self, x, y, zero_at_min=False):
-        (xmin, ymin) = (np.amin(x), np.amin(y)) if zero_at_min else (x[0],y[0])
+class HidensTransformation(AffineTransformation):
+    def __init__(self, x, y, zero_at_min=True):
+        if zero_at_min:
+            (xmin, ymin) = (np.amin(x), np.amin(y))
+        else:
+            (xmin, ymin) = (np.asscalar(x[0]),np.asscalar(y[0]))
+        xmin, ymin = 0, 0
+
         xspacing = np.median(np.unique(np.diff(np.sort(x))))
         yspacing = np.median(np.unique(np.diff(np.sort(y))))
         cart = np.array([[xmin,  xmin-xspacing, xmin+xspacing],
                          [ymin,  ymin+yspacing, ymin+yspacing]])
         hex = np.array( [[0,          1,         0],
                          [0,          0,         1]])
-        affine_transform.__init__(self, cart, hex)
+        logging.info('Initial transformation')
+        logging.info(cart)
+        AffineTransformation.__init__(self, cart, hex)
+        i, j = self.xy2ij(x,y, type=float)
+        x0, y0 = self.ij2xy(np.min(i),np.min(j))
+        xmin=np.asscalar(x0)
+        ymin=np.asscalar(y0)
+        cart = np.array([[xmin, xmin - xspacing, xmin + xspacing],
+                         [ymin, ymin + yspacing, ymin + yspacing]])
+        logging.info('Final transformation')
+        logging.info(cart)
+        self.match(cart, hex)
 
-    def xy2ij (self, x, y):
+    def xy2ij (self, x, y, type=int):
         hex = self.forward(np.array([x, y]))
-        i = np.round(hex[0, :]).astype(int)
-        j = np.round(hex[1, :]).astype(int)
+        hex = np.round(hex).astype(type) if type==int else hex
+        i = hex[0, :]
+        j = hex[1, :]
         return i, j
 
     def ij2xy(self, i,j):
@@ -69,35 +101,23 @@ def test_grid(neuron=5, coarse=True):
     filename = FIGURE_NEURON_FILE_FORMAT % neuron
     V, t, x, y, trigger, neuron = load_traces(filename)
 
-    xspacing = np.mean(np.diff(np.unique(np.sort(x))))
-    yspacing = np.mean(np.diff(np.unique(np.sort(y))))
+    hidens = HidensTransformation(x, y)
 
-    cart = np.array([[0, -xspacing, +xspacing],
-                     [0, yspacing, yspacing]])
+    i, j = hidens.xy2ij(x, y)
 
-    hex = np.array([[0, 1, 0],
-                    [0, 0, 1]])
+    x2, y2 = hidens.ij2xy(i, j)
 
-
-
-
-
-
-    cart2hex = transform_hidens_grid(x,y)
+    ax1 = plt.subplot(121)
+    plt.plot(x,y,'b+')
+    plt.plot(x2,y2,'kx')
+    x0, y0 = hidens.ij2xy(np.min(i), np.min(j))
+    plt.plot(x0,y0,'ro')
+    # set_axis_hidens(ax1)
 
 
-    print cart2hex.forward(cart)
-
-    i, j = cart2hex.xy2ij(x, y)
-
-    x, y = cart2hex.ij2xy(i, j)
-
-    plt.subplot(121)
-    plt.plot(x,y,'ko')
-
-
-    plt.subplot(122)
+    ax2 = plt.subplot(122)
     plt.plot(i, j, 'ko')
+    ax2.axis('equal')
 
     plt.show()
 
