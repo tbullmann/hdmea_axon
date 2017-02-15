ac0206053ab6d50cce18e49f5b187f736725b572
tbullmann <bullmann.torsten@gmail.com>
2017-02-15 20:18:23 +0900

    working figure

--- publication/figure10.py
+++ publication/figure10.py
@@ -4,6 +4,7 @@ logging.basicConfig(level=logging.DEBUG)
 import os as os
 import pandas as pd
 import numpy as np
+from scipy.spatial import distance
 from matplotlib import pyplot as plt
 
 
@@ -16,10 +17,9 @@ from publication.plotting import FIGURE_NEURON_FILE, without_spines_and_ticks, c
 from publication.comparison import segment_axon_Bakkum
 
 
+# Figure 10
 
-# Testing code
-
-def testing_load_traces(neuron):
+def figure10(neuron):
 
     path ='data/groundtruth/neuron%d' % neuron
 
@@ -30,53 +30,65 @@ def testing_load_traces(neuron):
 
     t = t/20 * 1000  # convert to ms TODO Fix factor 20
 
-    # Segmentation according Bakkum
-    delay_old, _, _, index_AIS, axon_pnr5 = segment_axon_Bakkum(V, t, pnr_threshold=5)
-    _, _, _, _, axon_pnr3 = segment_axon_Bakkum(V, t, pnr_threshold=3)
-
-
-
-    # Segmentation according Bullmann
-    neighbors = neighbors_from_electrode_positions(x, y)
-    delay, mean_delay, std_delay, expected_std_delay, thr, valid_delay, index_AIS, positive_delay, axon \
-        = segment_axon_verbose(t, V, neighbors)
-
-    from future_publication.grid import HidensTransformation
-    hidens = HidensTransformation(x,y)
-    xs, ys, Vs = hidens.subset(V, ioffset=0, joffset=1)
-    neighborss = neighbors_from_electrode_positions(xs, ys, neighborhood_radius=60)
-    delays, _, std_delay, _, _, _, _, _, _  = segment_axon_verbose(t, Vs, neighborss)
-    axon_2 = std_delay < thr  # using the threshold, because less data to compute histogram
-
     # AIS coordinates
+    index_AIS = find_AIS(V)
     x_AIS = x[index_AIS]
     y_AIS = y[index_AIS]
 
-    Vmin = np.min(V, axis=1)
-    Vmins = np.min(Vs, axis=1)
+    # Segmentation according Bakkum
+    axon_Bakkum = dict()
+    for pnr in (2, 3, 4, 5):
+        delay, _, _, _, axon = segment_axon_Bakkum(V, t, pnr_threshold=pnr)
+        axon_Bakkum[pnr] = axon
+    delay_Bakkum = delay
+    Vmin_Bakkum = np.min(V, axis=1)
+
+    # Segmentation according Bullmann
+    from future_publication.grid import HidensTransformation
+    axon_Bullmann = dict()
+    delay_Bullmann = dict()
+    Vmin_Bullmann = dict()
+    x_Bullmann = dict()
+    y_Bullmann = dict()
+    hidens = HidensTransformation(x,y)
+    for period in (1,2,3):
+        if period>1:
+            xs, ys, Vs = hidens.subset(V, period=period, ioffset=0, joffset=1)
+        else:
+            xs, ys, Vs = x, y, V
+        neighbors = neighbors_from_electrode_positions(xs, ys, neighborhood_radius=20*period)
+        delay, _, std_delay, _, thr, _, _, _, axon = segment_axon_verbose(t, Vs, neighbors)
+        if period==1:
+            keep_thr = thr
+        else:
+            axon = std_delay < keep_thr  # using the threshold, because less data to compute histogram
+        axon_Bullmann[period] = axon
+        delay_Bullmann[period] = delay
+        Vmin_Bullmann[period] = np.min(Vs, axis=1)
+        x_Bullmann[period] = xs
+        y_Bullmann[period] = ys
 
     # Plotting
-    fig = plt.figure('Figure X neuron %d' % neuron, figsize=(15, 11))
-    fig.suptitle('Figure X. Ground truth for neuron %d' % neuron, fontsize=14, fontweight='bold')
-    bbox = [200,800,1700,1200]
-
-    max_V = np.round(np.max(np.abs(x)),-3)
-
-    V2size = lambda x : np.abs(x)/np.sqrt(max_V) * 100
+    fig = plt.figure('Figure 10', figsize=(15, 11))
+    fig.suptitle('Figure 10. Haussdorf distance from ground truth for neuron %d' % neuron, fontsize=14, fontweight='bold')
+    bbox = [100,900,1800,1100]
+    V2size = lambda x : np.abs(x) * 2
 
     # Map axons for Bakkum's method, high threshold
     ax1 = plt.subplot(331)
-    plot_image_axon_delay_voltage(ax1, path+'axon', axon_pnr5, delay_old, Vmin, x, y, transform=V2size)
+    plot_image_axon_delay_voltage(ax1, path+'axon', axon_Bakkum[5], delay_Bakkum, Vmin_Bakkum, x, y, transform=V2size)
     cross_hair(ax1, x_AIS, y_AIS, color='red')
     set_axis_data(bbox)
     ax1.set_title('Method I')
+    plt.text(0.1, 0.9, r'$\mathsf{V_n>5\sigma_{V}}$', ha='left', va='center', transform=ax1.transAxes)
 
     # Map axons for Bullmann's method, grid spacing ~ 20um
     ax2 = plt.subplot(332)
-    ax2h = plot_image_axon_delay_voltage(ax2, path+'axon', axon, delay, Vmin, x, y, transform=V2size)
+    ax2h = plot_image_axon_delay_voltage(ax2, path+'axon', axon_Bullmann[1], delay_Bullmann[1], Vmin_Bullmann[1], x_Bullmann[1], y_Bullmann[1], transform=V2size)
     cross_hair(ax2, x_AIS, y_AIS, color='red')
     set_axis_data(bbox)
     ax2.set_title('Method II')
+    plt.text(0.1, 0.9, r'$\mathsf{r\approx20\mu m}$', ha='left', va='center', transform=ax2.transAxes)
 
     # Ground truth
     ax3 = plt.subplot(333)
@@ -87,15 +99,17 @@ def testing_load_traces(neuron):
 
     # Map axons for Bakkum's method, low threshold
     ax4 = plt.subplot(334)
-    plot_image_axon_delay_voltage(ax4, path+'axon', axon_pnr3, delay_old, Vmin, x, y, transform=V2size)
+    plot_image_axon_delay_voltage(ax4, path+'axon', axon_Bakkum[3], delay_Bakkum, Vmin_Bakkum, x, y, transform=V2size)
     cross_hair(ax4, x_AIS, y_AIS, color='red')
     set_axis_data(bbox)
+    plt.text(0.1, 0.9, r'$\mathsf{V_n>3\sigma_{V}}$', ha='left', va='center', transform=ax4.transAxes)
 
     # Map axons for Bullmann's method, grid spacing ~ 40um
     ax5 = plt.subplot(335)
-    plot_image_axon_delay_voltage(ax5, path+'axon', axon_2, delays, Vmins, xs, ys, transform=V2size)
+    plot_image_axon_delay_voltage(ax5, path+'axon', axon_Bullmann[2], delay_Bullmann[2], Vmin_Bullmann[2], x_Bullmann[2], y_Bullmann[2], transform=V2size)
     cross_hair(ax5, x_AIS, y_AIS, color='red')
     set_axis_data(bbox)
+    plt.text(0.1, 0.9, r'$\mathsf{r\approx40\mu m}$', ha='left', va='center', transform=ax5.transAxes)
 
     # Colorbar for A, B, D, E
     ax6 = plt.subplot(336)
@@ -117,15 +131,53 @@ def testing_load_traces(neuron):
                                     orientation='vertical')
     cbar.set_label(r'$\mathsf{\tau_{axon}\ [ms]}$', fontsize=14)
 
-    # Testing ground truth, reading xg, yg from the axon label file(s)
-    ax7 = plt.subplot(337)
-    plot_neuron_image(path+'axon')
+    # Reading groundtruth xg, yg from the axon label file(s)
     xg, yg = groundtruth_neuron_image(path + 'axon')
 
-    plt.scatter(xg,yg)
+    # Bakkum's method: Haussdorf distance vs. threshold
+    ax7 = plt.subplot(337)
+    xx = list()
+    yy = list()
+    for pnr in axon_Bakkum.keys():
+        xx.append(pnr)
+        yy.append(compare_with_groundtruth(x[axon_Bakkum[pnr]], y[axon_Bakkum[pnr]], xg, yg))
+    plt.plot (xx,yy,'o--')
+    plt.xlabel(r'$\mathsf{V_{thr}\ [\sigma_{V}]}$')
+    plt.ylabel(r'$\mathsf{H\ [\mu m]}$')
+    plt.ylim((0,400))
+    plt.xlim((6,1))
+    adjust_position(ax7, xshrink=0.01, yshrink=0.01)
+
+    # Bullmann's method: Haussdorf distance vs. spacing of electrodes in the hexagonal grid
+    ax8 = plt.subplot(338)
+    xx = list()
+    yy = list()
+    for period in axon_Bullmann.keys():
+        xx.append(period * 20)
+        yy.append(compare_with_groundtruth(x_Bullmann[period][axon_Bullmann[period]], y_Bullmann[period][axon_Bullmann[period]], xg, yg))
+    plt.plot(xx, yy, 'o--')
+    plt.xlabel(r'$\mathsf{r\ [\mu m]}$')
+    plt.ylabel(r'$\mathsf{H\ [\mu m]}$')
+    plt.ylim((0,400))
+    plt.xlim((0,80))
+    adjust_position(ax8, xshrink=0.01, yshrink=0.01)
+
+    label_subplot(ax1, 'A', xoffset=-0.04, yoffset=-0.015)
+    label_subplot(ax2, 'B', xoffset=-0.04, yoffset=-0.015)
+    label_subplot(ax3, 'C', xoffset=-0.04, yoffset=-0.015)
+    label_subplot(ax4, 'D', xoffset=-0.04, yoffset=-0.015)
+    label_subplot(ax5, 'E', xoffset=-0.04, yoffset=-0.015)
+    label_subplot(ax7, 'F', xoffset=-0.05, yoffset=-0.015)
+    label_subplot(ax8, 'G', xoffset=-0.05, yoffset=-0.015)
 
     plt.show()
 
+
+def compare_with_groundtruth(x, y, xg, yg):
+    C1 = np.array([x, y]).T
+    C2 = np.array([xg, yg]).T
+    distance = distanceBetweenCurves(C1, C2)
+    return distance
 
 def set_axis_data(bbox):
     set_bbox(bbox)
@@ -197,19 +249,19 @@ def distanceBetweenCurves(C1, C2):
     """
     From http://stackoverflow.com/questions/13692801/distance-matrix-of-curves-in-python
     Note: According to https://en.wikipedia.org/wiki/Hausdorff_distance this is defined
-    as max(H1, H2) and not (H1 + H2) / 2.
+    as max(H1, H2) and not (H1 + H2) / 2 as in the xode example
     :param C1, C2: to curves as their points
     :return:
     """
-    D = scipy.spatial.distance.cdist(C1, C2, 'euclidean')
+    D = distance.cdist(C1, C2, 'euclidean')
 
     #none symmetric Hausdorff distances
     H1 = np.max(np.min(D, axis=1))
     H2 = np.max(np.min(D, axis=0))
 
-    return (H1 + H2) / 2.
+    Hmax = np.max((H1 ,H2))
+    return Hmax
 
 
 if __name__ == "__main__":
-    testing_load_traces(1544)
-    # testing_load_traces(1536)
+    figure10(neuron=1544)   # TODO add ground truth for neuron 1536
