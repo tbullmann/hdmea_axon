e989d5da9e487d4d3f144e6e182d557b8a4ca6f3
tbullmann <bullmann.torsten@gmail.com>
2017-03-30 19:05:36 +0900

    Added supplemental figure showing summary plot for each culture

--- publication/all_figures.py
+++ publication/all_figures.py
@@ -1,17 +1,23 @@
+import data
 import figure_axon
 import figure_groundtruth
 import figure_comparison
 import figure_dendrite
 import figure_structural_and_functional
 import figure_structur_vs_function_and_synapses
+import figure_multiple_networks
 import figure_all_neurons
 import figure_more_structure_vs_function
 import figure_polychronous_groups
 
 if __name__ == "__main__":
+
+    data.extract_multiple_networks()
+
     figpath = 'figures'
 
-    figure_all_neurons.make_figure('Supplementary Figure 1', figpath=figpath)
+    figure_multiple_networks.make_figure('Supplementary Figure 1', figpath=figpath)
+    figure_all_neurons.make_figure('Supplementary Figure 2', figpath=figpath)
 
     figure_axon.make_figure('Figure1', figpath=figpath)
     figure_groundtruth.make_figure('Figure2', figpath=figpath)
@@ -21,4 +27,4 @@ if __name__ == "__main__":
     figure_structur_vs_function_and_synapses.make_figure('Figure6', figpath=figpath)
     figure_polychronous_groups.make_figure('Figure7', figpath=figpath)
 
-    figure_more_structure_vs_function.make_figure('Supplementary Figure 2', figpath=figpath)
+    figure_more_structure_vs_function.make_figure('Supplementary Figure 3', figpath=figpath)
