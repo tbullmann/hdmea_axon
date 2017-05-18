c14bb4371f28aa4e6558c154125ebbf98a0f7b35
tbullmann <bullmann.torsten@gmail.com>
2017-05-18 15:20:29 +0900

    Updated figures and movies

--- publication/all_figures.py
+++ publication/all_figures.py
@@ -13,14 +13,14 @@ if __name__ == "__main__":
 
     figpath = 'figures'
 
-    # figure_axon.make_figure('Figure1', figpath=figpath)
+    figure_axon.make_figure('Figure1', figpath=figpath)
     figure_groundtruth.make_figure('Figure2', figpath=figpath)
-    # figure_comparison.make_figure('Figure3', figpath=figpath)
-    # figure_dendrite.make_figure('Figure4', figpath=figpath)
-    # figure_structural_and_functional.make_figure('Figure5', figpath=figpath)
-    # figure_structur_vs_function_and_synapses.make_figure('Figure6', figpath=figpath)
-    # figure_polychronous_groups.make_figure('Figure7', figpath=figpath)
-    #
-    # figure_multiple_networks.make_figure('Supplementary Figure 1', figpath=figpath)
-    # figure_all_neurons.make_figure('Supplementary Figure 2', figpath=figpath)
-    # figure_more_structure_vs_function.make_figure('Supplementary Figure 3', figpath=figpath)
+    figure_comparison.make_figure('Figure3', figpath=figpath)
+    figure_dendrite.make_figure('Figure4', figpath=figpath)
+    figure_structural_and_functional.make_figure('Figure5', figpath=figpath)
+    figure_structur_vs_function_and_synapses.make_figure('Figure6', figpath=figpath)
+    figure_polychronous_groups.make_figure('Figure7', figpath=figpath)
+
+    figure_multiple_networks.make_figure('Supplementary Figure 1', figpath=figpath)
+    figure_all_neurons.make_figure('Supplementary Figure 2', figpath=figpath)
+    figure_more_structure_vs_function.make_figure('Supplementary Figure 3', figpath=figpath)
