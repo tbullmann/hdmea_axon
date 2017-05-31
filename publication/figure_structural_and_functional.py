74a5e298b20a19b3568e9ed028cde7497d256b3c
tbullmann <bullmann.torsten@gmail.com>
2017-05-31 17:12:43 +0900

    new functional/synaptic connectivity with control of false discovery rate in multiple comparisons by

--- publication/figure_structural_and_functional.py
+++ publication/figure_structural_and_functional.py
@@ -1,34 +1,38 @@
+from __future__ import division
+
 import logging
 import os
+
 import numpy as np
 from matplotlib import pyplot as plt
 
+from hana.function import timelag_standardscore, all_peaks
 from hana.misc import unique_neurons
 from hana.plotting import plot_neuron_points, plot_neuron_id, plot_neuron_pair, plot_network, mea_axes, highlight_connection
 from hana.recording import load_positions, average_electrode_area
 from hana.segmentation import neuron_position_from_trigger_electrode
 from hana.structure import find_overlap, all_overlaps
-from hana.function import timelag_standardscore, all_peaks
-
+from misc.figure_structural import plot_two_colorbars
 from publication.data import Experiment, FIGURE_CULTURE, FIGURE_NEURON, FIGURE_CONNECTED_NEURON, \
-    FIGURE_NOT_CONNECTED_NEURON, FIGURE_THRESHOLD_OVERLAP_AREA, FIGURE_THRESHOLD_Z_SCORE
-from publication.plotting import show_or_savefig, adjust_position, without_spines_and_ticks
+    FIGURE_NOT_FUNCTIONAL_CONNECTED_NEURON, FIGURE_NOT_STRUCTURAL_CONNECTED_NEURON, FIGURE_THRESHOLD_OVERLAP_AREA
 from publication.figure_functional import plot_std_score_and_peaks
-from publication.figure_structural import plot_two_colorbars
+from publication.plotting import show_or_savefig, adjust_position, without_spines_and_ticks
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 # Final version
 
-
 def make_figure(figurename, figpath=None):
 
-    # Neuron positions
+    # Neuron compartments and AIS positions
     trigger, _, axon_delay, dendrite_peak = Experiment(FIGURE_CULTURE).compartments()
     pos = load_positions(mea='hidens')
-    electrode_area = average_electrode_area(pos)
+    # electrode_area = average_electrode_area(pos)
     neuron_pos = neuron_position_from_trigger_electrode(pos, trigger)
+
+    # Structural and functional connectivity
+    structural_strength, structural_delay, functional_strength, functional_delay, _, _ = Experiment(FIGURE_CULTURE).networks()
 
     # Making figure
     fig = plt.figure(figurename, figsize=(13, 14))
@@ -36,16 +40,8 @@ def make_figure(figurename, figpath=None):
         fig.suptitle(figurename + ' Estimate structural and functional connectivity', fontsize=14, fontweight='bold')
     plt.subplots_adjust(left=0.10, right=0.95, top=0.90, bottom=0.05)
 
-    # Examples for structual connected and unconnected neurons and structural network
-    thr_overlap = np.ceil(FIGURE_THRESHOLD_OVERLAP_AREA / electrode_area)  # number of electrodes
-    logging.info('Overlap of at least %d um2 corresponds to %d electrodes.' % (FIGURE_THRESHOLD_OVERLAP_AREA, thr_overlap))
-    overlap, ratio, delay = find_overlap(axon_delay, dendrite_peak, FIGURE_NEURON, FIGURE_CONNECTED_NEURON,
-                                         thr_overlap=thr_overlap)
-    overlap2, ratio2, delay2 = find_overlap(axon_delay, dendrite_peak, FIGURE_NEURON, FIGURE_NOT_CONNECTED_NEURON,
-                                            thr_overlap=thr_overlap)
-
     ax1 = plt.subplot(421)
-    plot_neuron_pair(ax1, pos, axon_delay, dendrite_peak, neuron_pos, FIGURE_CONNECTED_NEURON, FIGURE_NEURON, delay)
+    plot_neuron_pair(ax1, pos, axon_delay, dendrite_peak, neuron_pos, FIGURE_CONNECTED_NEURON, FIGURE_NEURON, structural_delay[FIGURE_NEURON, FIGURE_CONNECTED_NEURON])
     mea_axes(ax1, style='off')
     ax1.set_title ('neuron pair %d $\longrightarrow$ %d' % (FIGURE_NEURON, FIGURE_CONNECTED_NEURON))
     plot_two_colorbars(ax1)
@@ -53,73 +49,62 @@ def make_figure(figurename, figpath=None):
     plt.title('a', loc='left', fontsize=18)
 
     ax2 = plt.subplot(423)
-    plot_neuron_pair(ax2, pos, axon_delay, dendrite_peak, neuron_pos, FIGURE_NOT_CONNECTED_NEURON, FIGURE_NEURON, delay2)
+    plot_neuron_pair(ax2, pos, axon_delay, dendrite_peak, neuron_pos, FIGURE_NOT_STRUCTURAL_CONNECTED_NEURON, FIGURE_NEURON, np.NaN)
     mea_axes(ax2, style='off')
-    ax2.set_title('neuron pair %d $\dashrightarrow$ %d' % (FIGURE_NEURON, FIGURE_NOT_CONNECTED_NEURON))
+    ax2.set_title('neuron pair %d $\dashrightarrow$ %d' % (FIGURE_NEURON, FIGURE_NOT_STRUCTURAL_CONNECTED_NEURON))
     plot_two_colorbars(ax2)
     adjust_position(ax2, yshrink=0.01)
     plt.title('b', loc='left', fontsize=18)
 
     # Whole network
     ax3 = plt.subplot(222)
-    all_overlap, all_ratio, all_delay = all_overlaps(axon_delay, dendrite_peak, thr_overlap=thr_overlap)
-    plot_neuron_points(ax3, unique_neurons(all_delay), neuron_pos)
+    # all_overlap, all_ratio, all_delay = all_overlaps(axon_delay, dendrite_peak, thr_overlap=thr_overlap)
+    plot_network (ax3, structural_delay, neuron_pos, color='gray')
+    plot_neuron_points(ax3, unique_neurons(structural_delay), neuron_pos)
     plot_neuron_id(ax3, trigger, neuron_pos)
-    plot_network (ax3, all_delay, neuron_pos)
-    highlight_connection(ax3, (FIGURE_NEURON, FIGURE_NOT_CONNECTED_NEURON), neuron_pos, connected=False)
+    highlight_connection(ax3, (FIGURE_NEURON, FIGURE_NOT_STRUCTURAL_CONNECTED_NEURON), neuron_pos, connected=False)
     highlight_connection(ax3, (FIGURE_NEURON, FIGURE_CONNECTED_NEURON), neuron_pos)
-    ax3.text(200, 250,r'$\mathsf{\rho=%3d\ \mu m^2}$' % FIGURE_THRESHOLD_OVERLAP_AREA, fontsize=18)
+    # ax3.text(200, 250,r'$\mathsf{\rho=%3d\ \mu m^2}$' % FIGURE_THRESHOLD_OVERLAP_AREA, fontsize=18)
     mea_axes(ax3)
     plt.title('c     structural connectivity graph', loc='left', fontsize=18)
 
-
     # Examples for functional connected and unconnected neurons and functional network
-
-    timeseries = Experiment(FIGURE_CULTURE).timeseries()
-    timeseries_surrogates = Experiment(FIGURE_CULTURE).timeseries_surrogates()
     timelags, std_score_dict, timeseries_hist_dict = Experiment(FIGURE_CULTURE).standardscores()
-    peak_score, peak_timelag, _, _ = all_peaks(timelags, std_score_dict, thr=FIGURE_THRESHOLD_Z_SCORE, direction='reverse')
+    peak_score, peak_timelag, z_threshold = all_peaks(timelags, std_score_dict)
 
-    # Plotting reverse
+    # Plotting forward
     ax4 = plt.subplot(224)
-    plot_network(ax4, peak_score, neuron_pos)
+    plot_network(ax4, peak_score, neuron_pos, color='gray')
     neuron_dict = unique_neurons(peak_score)
     plot_neuron_points(ax4, neuron_dict, neuron_pos)
     plot_neuron_id(ax4, neuron_dict, neuron_pos)
-    highlight_connection(ax4, (FIGURE_NEURON, FIGURE_NOT_CONNECTED_NEURON), neuron_pos, connected=False)
+    highlight_connection(ax4, (FIGURE_NEURON, FIGURE_NOT_FUNCTIONAL_CONNECTED_NEURON), neuron_pos, connected=False)
     highlight_connection(ax4, (FIGURE_NEURON, FIGURE_CONNECTED_NEURON), neuron_pos)
-    ax4.text(200, 250,r'$\mathsf{\zeta=%d}$' % FIGURE_THRESHOLD_Z_SCORE, fontsize=18)
     mea_axes(ax4)
     plt.title('f     functional connectivity graph', loc='left', fontsize=18)
 
-
     ax5 = plt.subplot(425)
-    plot_z_score (ax5, FIGURE_NEURON, FIGURE_CONNECTED_NEURON, FIGURE_THRESHOLD_Z_SCORE, peak_timelag, timeseries, timeseries_surrogates)
+    plot_z_score (ax5, FIGURE_NEURON, FIGURE_CONNECTED_NEURON, z_threshold, peak_timelag, timelags, std_score_dict)
     ax5.set_title ('neuron pair %d $\longrightarrow$ %d' % (FIGURE_NEURON, FIGURE_CONNECTED_NEURON))
     without_spines_and_ticks(ax5)
     plt.title('d', loc='left', fontsize=18)
-
     ax6 = plt.subplot(427)
-    plot_z_score (ax6, FIGURE_NEURON, FIGURE_NOT_CONNECTED_NEURON, FIGURE_THRESHOLD_Z_SCORE, peak_timelag, timeseries, timeseries_surrogates)
-    ax6.set_title ('neuron pair %d $\longrightarrow$ %d' % (FIGURE_NEURON, FIGURE_NOT_CONNECTED_NEURON))
+    plot_z_score (ax6, FIGURE_NEURON, FIGURE_NOT_FUNCTIONAL_CONNECTED_NEURON, z_threshold, peak_timelag, timelags, std_score_dict)
+    ax6.set_title ('neuron pair %d $\longrightarrow$ %d' % (FIGURE_NEURON, FIGURE_NOT_FUNCTIONAL_CONNECTED_NEURON))
     without_spines_and_ticks(ax6)
     plt.title('e', loc='left', fontsize=18)
-
-    # plot_func_example_and_network(ax4, ax5, ax6, presynaptic_neuron, postsynaptic_neuron, 'reverse', thr, neuron_pos, std_score_dict,
-    #                               timelags, timeseries, timeseries_surrogates)
     adjust_position(ax5, yshrink=0.01, xshrink=0.04)
     adjust_position(ax6, yshrink=0.01, xshrink=0.04)
-    ax5.set_ylim((-FIGURE_THRESHOLD_Z_SCORE, FIGURE_THRESHOLD_Z_SCORE * 3))
-    ax6.set_ylim((-FIGURE_THRESHOLD_Z_SCORE, FIGURE_THRESHOLD_Z_SCORE * 3))
+    ax5.set_ylim((-5, 15))
+    ax6.set_ylim((-5, 15))
 
     show_or_savefig(figpath, figurename)
 
-def plot_z_score (ax3, pre, post, thr, peak_timelag, timeseries, timeseries_surrogates):
-    timelags, std_score, timeseries_hist, surrogates_mean, surrogates_std \
-        = timelag_standardscore(timeseries[post], timeseries[pre],
-                                timeseries_surrogates[pre])  # calculate for network
+
+def plot_z_score (ax3, pre, post, thr, peak_timelag, timelags, std_score_dict):
+    std_score = std_score_dict[(pre, post)]
     if (pre, post) in peak_timelag:
-        peak = -peak_timelag[(pre, post)]
+        peak = peak_timelag[(pre, post)]
     else:
         peak = None
     plot_std_score_and_peaks(ax3, timelags, std_score, thr=thr, peak=peak, loc=0)
---
+++ publication/figure_synapses.py
@@ -0,0 +1,210 @@
+from __future__ import division
+
+import logging
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from hana.misc import unique_neurons
+from hana.plotting import plot_network, plot_neuron_points, mea_axes, plot_neuron_id
+from hana.recording import average_electrode_area
+from hana.segmentation import load_positions, neuron_position_from_trigger_electrode
+from publication.data import Experiment, FIGURE_CULTURE, FIGURE_CULTURES
+from publication.plotting import show_or_savefig, plot_synapse_delays, adjust_position, \
+    without_spines_and_ticks
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def make_figure(figurename, figpath=None):
+
+    structural_strength, structural_delay, functional_strength, functional_delay, synapse_strength, synapse_delay \
+        = Experiment(FIGURE_CULTURE).networks()
+
+    timelags, std_score_dict, timeseries_hist_dict = Experiment(FIGURE_CULTURE).standardscores()
+    minimal_synaptic_delay = 1.0
+
+    # Map electrode number to area covered by that electrodes
+    electrode_area = average_electrode_area(None, mea='hidens')
+
+    # Making figure
+    fig = plt.figure(figurename, figsize=(13, 13))
+    if not figpath:
+        fig.suptitle(figurename + ' Estimate synaptic connectivity', fontsize=14, fontweight='bold')
+    plt.subplots_adjust(left=0.10, right=0.92, top=0.90, bottom=0.05)
+
+    remaining_keys = [key for key in structural_delay.keys() if not key in synapse_delay.keys()]
+
+    ax12 = plt.subplot(421)
+    postsynaptic = timelags > 0
+    for i, (pair, axonal_delay) in enumerate(sorted(structural_delay.items(), key=lambda x: x[1])):
+        x = timelags[postsynaptic] - axonal_delay
+        y = i + std_score_dict[pair][postsynaptic]
+        xx = np.hstack((x[0], np.ravel(zip(x[:-1], x[1:])) , x[-1]))
+        yy = np.hstack((i,    np.ravel(zip(y[:-1], y[:-1])), i))
+        plt.fill_between(xx, 0, yy, color='white', zorder=-i, clip_on=False)
+        plt.plot(xx, yy, color='black', zorder=-i, clip_on=False)
+        plt.plot(-axonal_delay, i, 'b.')
+        if pair in synapse_delay:
+            plt.vlines(synapse_delay[pair] - axonal_delay +0.05, i, i+synapse_strength[pair], 'green', linewidth=5, zorder=-i)
+    without_spines_and_ticks(ax12)
+    ax12.set_ylim((-10,250))
+    ax12.set_xlim((-2.2,5))
+    ax12.spines['bottom'].set_bounds(-2, 5)
+    ax12.spines['left'].set_bounds(0, 100)
+    ax12.set_yticks([0,50,100])
+    ax12.set_yticklabels([0,50,100])
+    ax12.set_ylabel(r'$\mathsf{z_{max}}$', fontsize=14)
+    ax12.yaxis.set_label_coords(-0.1, 0.25)
+    ax12.set_xlabel(r'$\mathsf{\tau_{synapse}=\tau_{spike}-\tau_{axon}\ [ms]}$', fontsize=14)
+    plt.title('a', loc='left', fontsize=18)
+
+    ax111 = plt.subplot(423)
+    for i, (pair, axonal_delay) in enumerate(sorted(structural_delay.items(), key=lambda x: x[1])):
+        plt.plot(-axonal_delay, i, 'b.')
+        if pair in synapse_delay:
+            plt.plot(synapse_delay[pair]-axonal_delay,i, 'g.')
+    ax111.set_ylabel(r'$\mathsf{structural\ connection\ rank\ by\ \tau_{axon}}$', fontsize=14)
+    ax111.fill([0, minimal_synaptic_delay, minimal_synaptic_delay, 0], [0, 0, 250, 250], fill=False, hatch='\\',
+               linewidth=0)
+    ax111.set_ylim((0, 220))
+    ax111.set_xticks([-2, -1, 0, 1, 2, 3, 4, 5])
+    ax111.set_xticklabels([2, 1, 0, 1, 2, 3, 4, 5])
+    without_spines_and_ticks(ax111)
+    ax111.set_xlabel(r'$\mathsf{\tau_{axon}\ [ms]\ \leftarrow\ \ \ \ \ \rightarrow\tau_{synapse}\ [ms]}$', fontsize=14)
+    ax111.xaxis.set_label_coords(0.31, -0.1)
+    ax111.spines['bottom'].set_bounds(-2, 5)
+    ax111.set_xlim((-2.2,5))
+    adjust_position(ax111, yshrink=0.01, yshift=-0.01)
+    plt.title('b', loc='left', fontsize=18)
+
+    ax2 = plt.subplot(222)
+    trigger, _, _, _ = Experiment(FIGURE_CULTURE).compartments()
+    pos = load_positions(mea='hidens')
+    neuron_pos = neuron_position_from_trigger_electrode(pos, trigger)
+    plot_network(ax2, synapse_delay.keys(), neuron_pos, color='gray')
+    plot_network(ax2, remaining_keys, neuron_pos, color='green')
+    plot_neuron_points(ax2, unique_neurons(structural_delay), neuron_pos)
+    plot_neuron_id(ax2, trigger, neuron_pos)
+    # Legend by proxy
+    ax2.hlines(0, 0, 0, linestyle='-', color='gray', label='none')
+    ax2.hlines(0, 0, 0, linestyle='-', color='green', label='synapse')
+    plt.legend(frameon=False)
+    mea_axes(ax2)
+    adjust_position(ax2, yshift=-0.01)
+    plt.title('c     synaptic connectivity graph', loc='left', fontsize=18)
+
+    # make summaries
+    data = []
+    synapse_delays = list()
+    for culture in FIGURE_CULTURES:
+        structural_strength, structural_delay, _, _, synapse_strength, synapse_delay \
+            = Experiment(culture).networks()
+        culture_data = DataFrame_from_Dicts(structural_delay, structural_strength, synapse_delay)
+        culture_data['culture'] = culture
+        data.append(culture_data)
+        synapse_delays.append(synapse_delay.values())
+    data = pd.concat(data)
+
+    connections = pd.pivot_table(data, index='culture', columns='delayed',
+                                 values='synaptic_delay', aggfunc = lambda x: len(x))
+    connections.fillna(0, inplace=True)
+    connections['delayed'] = 100* connections[True]/(connections[True] + connections[False])
+    connections['simultaneous'] = 100* connections[False] / (connections[True] + connections[False])
+
+    ax3 = plt.subplot(256)
+    bplot = ax3.boxplot(np.array(connections[['delayed','simultaneous']]),
+                        boxprops={'color': "black", "linestyle": "-"}, patch_artist=True,widths=0.7)
+    prettify_boxplot(ax3, bplot)
+    ax3.set_ylim((0,100))
+    ax3.set_ylabel('percent of structural connections [%]')
+    adjust_position(ax3, xshift=-0.02)
+    plt.title('d', loc='left', fontsize=18)
+
+    values = pd.pivot_table(data, index='culture', columns='delayed',
+                        values=['functional_strength', 'structural_strength', 'structural_delay'], aggfunc=np.median)
+
+    ax4 = plt.subplot(257)
+    bplot = ax4.boxplot(np.array(values['structural_strength']) * electrode_area,   # overlap in um2
+                        boxprops={'color': "black", "linestyle": "-"}, patch_artist=True, widths=0.7)
+    prettify_boxplot(ax4, bplot)
+    ax4.set_ylabel(r'$\mathsf{|A \cap D|\ [\mu m^2]}$', fontsize=14)
+    plt.title('e', loc='left', fontsize=18)
+
+    ax5 = plt.subplot(258)
+    bplot = ax5.boxplot(np.array(values['structural_delay']),
+                        boxprops={'color': "black", "linestyle": "-"}, patch_artist=True, widths=0.7)
+    prettify_boxplot(ax5, bplot)
+    ax5.set_ylim((0,2))
+    ax5.set_ylabel(r'$\mathsf{\tau_{axon}\ [ms]}$', fontsize=14)
+    plt.title('f', loc='left', fontsize=18)
+
+    ax6 = plt.subplot(259)
+    # show distirbution of median synapse delays as boxplot
+    bplot = plt.boxplot([np.median(x) for x in synapse_delays], 0, '+', 0, positions=np.array([0.]),
+                        boxprops={'color': "black", "linestyle": "-"}, patch_artist=True, widths=0.7)
+    bplot['boxes'][0].set_color('green')
+    plt.setp(bplot['medians'], color='black')
+    plt.setp(bplot['whiskers'], color='black')
+    # add violin plot for every culture
+    vplot = plt.violinplot(synapse_delays, FIGURE_CULTURES, points=80, vert=False, widths=0.7,
+                           showmeans=False, showextrema=True, showmedians=True)
+    ax6.fill([0, minimal_synaptic_delay, minimal_synaptic_delay, 0], [-1, -1, 9, 9],
+             fill=False, hatch='\\', linewidth=0)
+
+    # Make all the violin statistics marks black:
+    for partname in ('cbars', 'cmins', 'cmaxes', 'cmedians'):
+        vp = vplot[partname]
+        vp.set_edgecolor('black')
+        vp.set_linewidth(1)
+    # Make the violin body blue with a black border:
+    for pc in vplot['bodies']:
+        pc.set_facecolor('green')
+        pc.set_alpha(0.5)
+        pc.set_edgecolor('none')
+    ax6.set_xlabel(r'$\mathsf{\tau_{synapse}\ [ms]}$', fontsize=14)
+    ax6.set_xticks([0, 1, 2, 3, 4, 5])
+    ax6.set_xticklabels([0, 1, 2, 3, 4, 5])
+    ax6.set_ylabel('culture')
+    ax6.set_yticks([0,]+list(FIGURE_CULTURES))
+    ax6.set_yticklabels(['all',]+list(FIGURE_CULTURES))
+    ax6.set_ylim((-0.5,6.5))
+    ax6.invert_yaxis()
+    adjust_position(ax6, xshift=0.08, xshrink=-0.05, yshrink=0.04)
+    without_spines_and_ticks(ax6)
+    plt.title('g', loc='left', fontsize=18)
+
+    show_or_savefig(figpath, figurename)
+
+
+def prettify_boxplot(ax, bplot):
+    colors = ['green', 'gray']
+    for patch, color in zip(bplot['boxes'], colors):
+        patch.set_color(color)
+    plt.setp(bplot['medians'], color='black')
+    plt.setp(bplot['whiskers'], color='black')
+    plt.xlabel(r'$\mathsf{\tau_{synapse}\ [ms]}$', fontsize=14)
+    ax.set_xticks([1, 2])
+    ax.set_xticklabels(['synapse', 'none'])
+    adjust_position(ax,xshrink=0.02, yshrink=0.04)
+    without_spines_and_ticks(ax)
+
+
+def DataFrame_from_Dicts(structural_delay, structural_strength, synapse_delay):
+    data = pd.merge(pd.merge(pd.DataFrame(((pre, post, value) for (pre, post), value in structural_delay.items()),
+                                          columns=['pre', 'post', 'structural_delay']),
+                             pd.DataFrame(((pre, post, value) for (pre, post), value in structural_strength.items()),
+                                          columns=['pre', 'post', 'structural_strength']),
+                             on=['pre', 'post']),
+                    pd.DataFrame(((pre, post, value) for (pre, post), value in synapse_delay.items()),
+                                 columns=['pre', 'post', 'synaptic_delay']),
+                    on=['pre', 'post'], how='outer')
+    data.fillna(0, inplace=True)   # zero synaptic delay for the connection not assigned a synapse
+    data['delayed'] = data.synaptic_delay > 1  # ms
+    data['simultaneous'] = data.synaptic_delay < 1  # ms
+    return data
+
+
+if __name__ == "__main__":
+    make_figure(os.path.basename(__file__))
