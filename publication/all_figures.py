a1e0f529f89767a7f3fda510728d987f19272688
tbullmann <bullmann.torsten@gmail.com>
2019-03-20 11:08:04 +0100

    Figures for Publication

--- publication/all_figures.py
+++ publication/all_figures.py
@@ -3,15 +3,13 @@ import figure_axon
 import figure_groundtruth
 import figure_comparison
 import figure_all_neurons
-import figure_dendrite
 
 if __name__ == "__main__":
 
     figpath = 'figures'
 
     figure_footprint.make_figure('Figure2', figpath=figpath)
-    # figure_axon.make_figure('Figure3', figpath=figpath)
-    # figure_groundtruth.make_figure('Figure4', figpath=figpath)
-    # figure_comparison.make_figure('Figure5', figpath=figpath)
-    # figure_all_neurons.make_figure('Figure6', figpath=figpath) #Should be more an example
-    # figure_dendrite.make_figure('Figure7', figpath=figpath)
+    figure_axon.make_figure('Figure3', figpath=figpath)
+    figure_groundtruth.make_figure('Figure4', figpath=figpath)
+    figure_comparison.make_figure('Figure5', figpath=figpath)
+    figure_all_neurons.make_figure('Figure6', figpath=figpath)
